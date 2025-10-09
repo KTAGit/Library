1d6b7778b4865e6191ec2402e598031d2eb1eefe
KTAGit <121195747+KTAGit@users.noreply.github.com>
2025-10-09 17:00:42 -0400

    Add function to create a new Book instance and add it to the library

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
+// Array to store all book object
+const myLibrary = []
 
+// Book class to define the structure and behavior of a book
 class Book {
-    constructor(title, author, pages, hasRead){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.hasRead = hasRead
+    constructor(title, author, pages, hasRead) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.hasRead = hasRead;
+            this.id = crypto.randomUUID();
     }
 
-    info(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "read" : "not read"}.`
-    }
+    info() {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "read" : "not read"}.`;
+        }
+}
+
+// Function to create a new Book instance and add it to the library
+function addBookToLibrary(title, author, pages, hasRead) {
+    let newBook = new Book(title, author, pages, hasRead)
+    myLibrary.push(newBook)
 }
 
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
 
-console.log(theHobbit.info())
+
+console.log(myLibrary);
